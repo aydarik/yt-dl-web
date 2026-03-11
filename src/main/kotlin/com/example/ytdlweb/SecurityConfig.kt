2b43fc7eef67e39b9d059834217b9a53cc2011a5
aydar <aydar.gumerbaev@friday.de>
2026-03-11 19:27:25 +0100

    users

--- src/main/kotlin/com/example/ytdlweb/SecurityConfig.kt
+++ src/main/kotlin/com/example/ytdlweb/SecurityConfig.kt
@@ -2,10 +2,12 @@ package com.example.ytdlweb
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
@@ -28,12 +30,20 @@ class SecurityConfig {
     }
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        val user = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("admin")
-            .roles("USER")
-            .build()
-        return InMemoryUserDetailsManager(user)
+    fun userDetailsService(env: Environment): UserDetailsService {
+
+        val usersEnv = env.getProperty("APP_USERS") ?: "admin:admin"
+        val users: List<UserDetails> = usersEnv.split(",")
+            .filter { it.isNotBlank() }
+            .map {
+                val (username, password) = it.split(":")
+                User.withDefaultPasswordEncoder()
+                    .username(username)
+                    .password(password)
+                    .roles("USER")
+                    .build()
+            }
+
+        return InMemoryUserDetailsManager(users)
     }
 }
